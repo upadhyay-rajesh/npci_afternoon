3ac9a19f1ef5627deda52acabd1f6ac9039f4648
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2025-01-10 17:55:20 +0530

    xszcxvc

--- mybankingconsumer/src/main/java/com/mybankingconsumer/controller/MyConsumerController.java
+++ mybankingconsumer/src/main/java/com/mybankingconsumer/controller/MyConsumerController.java
@@ -2,6 +2,9 @@ package com.mybankingconsumer.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +16,24 @@ import com.mybankingconsumer.dto.Employee;
 @RequestMapping("/api/v1/consumers")
 public class MyConsumerController {
 	
+	@Autowired
+	DiscoveryClient ds;
+	
 	@GetMapping
 	public List<Employee> getAll(){
+		
+		
+		List<ServiceInstance> ll= ds.getInstances("MYBANKING");
+		
+		ServiceInstance myinstance= ll.get(0);
+		
+		String uri=myinstance.getUri().toString();
+		
+		
+		
 		RestTemplate rr=new RestTemplate();
 		
-		return rr.getForObject("http://localhost:10000/api/v1/employees", List.class);
+		return rr.getForObject(uri+"/api/v1/employees", List.class);
 	}
 
 }
