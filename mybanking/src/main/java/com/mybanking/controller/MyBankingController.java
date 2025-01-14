beaef444b3923e10a7f3c9f20eb025802313f205
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2025-01-14 13:18:25 +0530

    sff

--- mybanking/src/main/java/com/mybanking/controller/MyBankingController.java
+++ mybanking/src/main/java/com/mybanking/controller/MyBankingController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/v1/employees")
 public class MyBankingController {
 	
